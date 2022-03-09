b488bc537268b180936d1ca7f51a85574e929291
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-09 06:03:56 +0530

    Switch statement practice programs

---
+++ Switch program/src/Demo5.java
@@ -0,0 +1,20 @@
+//Demo on switch
+public class Demo5 {
+	public static void main(String[] args) {
+	char ch = 'i';
+	switch(ch)
+	{
+	case 'a' : System.out.println("It is vowel");
+	break;
+	case 'e' : System.out.println("It is vowel");
+	break;
+	case 'i' : System.out.println("It is vowel");
+	break;
+	case 'o' : System.out.println("It is vowel");
+	break;
+	case 'u' : System.out.println("It is vowel");
+	break;
+	default : System.out.println("Not vowel");
+	}
+	}
+}
