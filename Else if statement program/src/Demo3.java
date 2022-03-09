c04836b5e68f3f65ca265a4fcec9ab52f9f1d10c
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-09 06:00:12 +0530

    Else if statement practice programs

---
+++ Else if statement program/src/Demo3.java
@@ -0,0 +1,18 @@
+//demo on else if
+public class Demo3{
+	public static void main(String[] args) {
+	int a = 0;
+	if(a>0)
+	{
+    System.out.println("It is positive number");
+	}
+	else if (a<0)
+	{
+	System.out.println("It is Negative number");
+	}
+	else
+	{	
+	System.out.println("It is zero");
+	}
+  }
+}
