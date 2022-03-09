25124520e25179bce47810d56ef028389a024c13
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-09 05:57:00 +0530

    If statement practice program

---
+++ If statement program/If else statement program/src/Demo2.java
@@ -0,0 +1,14 @@
+public class Demo2 {
+	public static void main(String[] args) {
+	int a = 100 , b = 150;
+	if (a>b)
+	{
+	System.out.println("a is big");
+	}
+	else {
+	System.out.println("b is big");
+	}
+	System.out.println("Process done");
+	}
+}
+
---
+++ If statement program/src/Demo1.java
@@ -0,0 +1,11 @@
+
+public class Demo1 {
+	public static void main(String[] args) {
+	int a = 10 , b = 5;
+	if (a>b)
+	{
+	System.out.println("a is big");
+	}
+	System.out.println("Process done");
+	}
+}
